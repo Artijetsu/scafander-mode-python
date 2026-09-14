c73f1baec08f430836297663b9831ef39229f59a
ARTIJETSU <buhankacopyright@gmail.com>
2026-09-14 09:23:48 +0300

    version 0.0.3
      Приложение может сохранять задачи, выдает список задач, и может удалять и редактировтаь задачи

--- DOCUMENTATION/main.py
+++ DOCUMENTATION/main.py
@@ -1,20 +1,53 @@
 """Основной файл приложения
-    версия 0.0.2
+
+    версия 0.0.3
+
+    === Описание ===
+        Приложение может сохранять задачи, выдает список задач, и может удалять и редактировтаь задачи
+
 """
 from random import choice
 
-collection = ['task1', 'task2', 'task3']
-is_start = True
+collection = ['task1', 'task2'] #list of tusks
+is_running = True
 
-while (is_start):
-    print("1 - Показать задачи / 2 - Добавить заметку")
-    choice_user = input("Введите ваш выбор (1 или 2): ")
+def show_collection(task_collection):
+    print("=" * 45)
+    for i, j in enumerate(collection):
+        print(i + 1, j)
+    print("=" * 45)
 
-    match choice_user:
-        case 1:
-            print(collection)
-        case 2:
-            collection.append('task')
-            print(collection)
+def show_menu():
+    print("1 - Показать задачи \n"
+          "2 - Добавить задачу \n"
+          "3 - Редактировать задачи \n"
+          "4 - Удаление задачи \n"
+          "5 - Выход")
+
+while (is_running):
+    choice_user = input('Введите ваш выбор: ')
+    match str(choice_user):
+        case "1":
+            show_collection(collection)
+
+        case "2":
+            add_tusk = input("Введите имя задачи для добавления: ")
+            collection.append(add_tusk)
+
+        case "3":
+            show_collection(collection)
+            select_tusk = int(input("Введите номер задачи: "))
+            edit_tusk = input("Введите новое имя задачи для редактирования: ")
+            collection[select_tusk - 1] = edit_tusk
+
+        case "4":
+            show_collection(collection)
+            delete_tusk = int(input("Введите номер задачи для удаления: "))
+            collection.pop(delete_tusk - 1)
+
+        case "5":
+            is_running = False
+            print("До свидиния!")
+
         case _:
-            print("Такого пункта нет!")
+            print('Такого пункта нет...')
